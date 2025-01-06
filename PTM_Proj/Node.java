3b5fe32de0b5ec9e769951cb123d20a8c6227932
Abraham Tassao <avrahamt001@gmail.com>
2025-01-06 18:06:23 +0200

    Update Node.java

--- PTM_Proj/Node.java
+++ PTM_Proj/Node.java
@@ -1,4 +1,4 @@
-package test;
+//package test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +9,37 @@ public class Node {
     private List<Node> edges;
     private Message msg;
 
+    public Node(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Node> getEdges() {
+        return edges;
+    }
+    public void setEdges(List<Node> edges) {
+        this.edges = edges;
+    }
+    public Message getMsg() {
+        return msg;
+    }
+    public void setMsg(Message msg) {
+        this.msg = msg;
+    }
 
+    public void addEdge(Node added) {
+        edges.add(added);
+    }
+    public boolean hasCycles() {
+        Node current = this;
+        for (Node node : edges) {
+            if (node.hasCycles()) {}
+        }
+        return false;
+    }
 
 }
